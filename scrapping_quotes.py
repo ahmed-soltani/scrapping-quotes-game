c874e4bcfcdc2d02a629edf7c0f8db511f604fb2
Ahmed Soltani <ahmed.soltani2026@gmail.com>
2023-09-17 15:15:47 +0200

    Part 1

---
+++ scrapping_quotes.py
@@ -0,0 +1,26 @@
+import requests 
+from bs4 import BeautifulSoup
+from time import sleep
+
+all_quotes = []
+base_url ="http://quotes.toscrape.com"
+url = "/page/1"
+
+while url:
+	res = requests.get(f"{base_url}{url}")
+	print(f"Now Scraping {base_url}{url}....")
+	soup = BeautifulSoup(res.text,"html.parser")
+	quotes = soup.find_all(class_="quote")
+
+	for quote in quotes:
+		all_quotes.append({
+			"text":quote.find(class_="text").get_text(),
+			"author": quote.find(class_="author").get_text(),
+			"bio-link": quote.find("a")["href"]
+			})
+	next_btn = soup.find(class_="next")
+	url = next_btn.find("a")["href"] if next_btn else None
+	sleep(3)
+print(all_quotes)
+
+#print(all_quotes)
